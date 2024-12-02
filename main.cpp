7b1bb00170f709e20ace849984e05598832b4ef3
kashef7 <11410120230477@stud.cu.edu.eg>
2024-12-02 17:41:49 +0200

    edit the main

--- main.cpp
+++ main.cpp
@@ -7,29 +7,7 @@
 using namespace std;
 #include <string>
 int main() {
-    Four_in_a_Row<char> *board = new Four_in_a_Row<char>();
-    char i ='o';
-    board->update_board(4, 0, 'O'); // Supporting piece
-    board->update_board(3, 0, 'X');
-
-    board->update_board(4, 1, 'O'); // Supporting piece
-    board->update_board(3, 1, 'O'); // Supporting piece
-    board->update_board(2, 1, 'X');
-
-    board->update_board(4, 2, 'O'); // Supporting piece
-    board->update_board(3, 2, 'O'); // Supporting piece
-    board->update_board(2, 2, 'O'); // Supporting piece
-    board->update_board(1, 2, 'X');
-
-    board->update_board(4, 3, 'C'); // Supporting piece
-    board->update_board(3, 3, 'O'); // Supporting piece
-    board->update_board(2, 3, 'O'); // Supporting piece
-    board->update_board(1, 3, 'C'); // Supporting piece
-    board->update_board(0, 3, 'C');
-
-    board->display_board();
-    if (board->is_win()) {
-        cout << "You win" << endl;
-    }
+    //Four_in_a_Row<char> *board = new Four_in_a_Row<char>();
+    cout << "hello";
 
 }
