59b2045d46c8cd17f28363ec2600af9a7a2b85d8
yousefashraf35 <youash335@gmail.com>
2024-12-04 20:28:43 +0200

    tany le3ba l yousef done

--- main.cpp
+++ main.cpp
@@ -23,25 +23,19 @@ int main() {
     for (int i = 0; i < 2; ++i) {
         delete players[i];
     }*/
-
-        // Create board
-        Numerical_TicTacToe<char> *board = new Numerical_TicTacToe<char>();
-
-        // Create players
-        Player<char>* players[2];
-        players[0] = new Numerical_TicTacToe_Player<char>("Player 1", '5');  // First player starts with 5
-        players[1] = new Numerical_TicTacToe_Player<char>("Player 2", '2');  // Second player uses 2
-
-        // Run game
-        GameManager<char> Numerical_TicTacToe_Game(board, players);
-        Numerical_TicTacToe_Game.run();
-
-        // Clean up memory
-        delete board;
-        for (int i = 0; i < 2; ++i) {
-            delete players[i];
-        }
-
-        return 0;
+    Numerical_TicTacToe<char> *board = new Numerical_TicTacToe<char>();
+    // Create players
+    Player<char>* players[2];
+    players[0] = new Numerical_TicTacToe_Player<char>("Player 1", '5');  // First player starts with 5
+    players[1] = new Numerical_TicTacToe_Player<char>("Player 2", '2');  // Second player uses 2
+    // Run game
+    GameManager<char> Numerical_TicTacToe_Game(board, players);
+    Numerical_TicTacToe_Game.run();
+    // Clean up memory
+    delete board;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+    return 0;
     cout << "zabat el menu ^_^";
 }
