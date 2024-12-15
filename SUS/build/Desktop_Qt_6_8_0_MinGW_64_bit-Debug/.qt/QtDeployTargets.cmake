4c5e164e5fbd32deb6ec51868d7323e4a6dba787
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-12-15 23:31:10 +0200

    Delete SUS/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug directory

--- SUS/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Sus_FILE D:/FCAI-CU/YEAR 2/SEMSTER 1/BoardGameAss/SUS/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Sus.exe)
-set(__QT_DEPLOY_TARGET_Sus_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Sus_RUNTIME_DLLS D:/qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/qt/6.8.0/mingw_64/bin/Qt6Core.dll)
