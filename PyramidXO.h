24fdaca71eba031eed522bcf9d54d0f193d2f52a
kashef7 <11410120230477@stud.cu.edu.eg>
2024-12-16 15:06:19 +0200

    defence for PyramidXO.h

--- PyramidXO.h
+++ PyramidXO.h
@@ -151,11 +151,33 @@ PyramidXO_Player<T>::PyramidXO_Player(string n,T symbol):Player<T>(n,symbol){}
 
 template <typename T>
 void PyramidXO_Player<T>::getmove(int &x, int &y) {
-    cout << this->name << "'s Turn " <<endl;
-    cout << "Enter x:" << endl;
-    cin >> x;
-    cout << "Enter y:" << endl;
-    cin >> y;
+    cout << this->name << "'s Turn " << endl;
+
+    while (true) {
+        cout << "Enter x: ";
+        cin >> x;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter an int" << endl;
+            continue;
+        } else {
+            break;
+        }
+    }
+    while (true) {
+        cout << "Enter y: ";
+        cin >> y;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Please enter an int" << endl;
+            continue;
+        } else {
+            break;
+        }
+    }
 }
 
 
