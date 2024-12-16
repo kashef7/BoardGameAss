a99f6efb8626a02439849868f30eabffb7580383
salmasherif04 <86133115+salmasherif04@users.noreply.github.com>
2024-12-16 15:55:27 +0200

    Add files via upload
    game 3 and 6 header file

--- Game3.h
+++ Game3.h
@@ -121,89 +121,125 @@ bool TicTacToeBoard<T>::game_is_over() {
     return is_win() || is_draw();
 }
 
-/*template <typename T>
-class TicTacToeBoard : public Board<char> {
+
+template <typename T>
+class MisereTicTacToeBoard : public Board<T>{
 public:
-    TicTacToeBoard() {
+    MisereTicTacToeBoard();
 
+    ~MisereTicTacToeBoard();
 
-    ~TicTacToeBoard() {
-        for (int i = 0; i < rows; ++i) {
-            delete[] board[i];
+    void display_board();
+
+    bool update_board(int x,int y,T symbol);
+
+    bool is_win();
+
+    bool is_draw();
+
+    bool game_is_over();
+
+};
+template <typename T>
+MisereTicTacToeBoard<T>::MisereTicTacToeBoard() {
+    this->rows = this->columns = 3;;
+    this->board = new char*[this->rows];
+    this->board = new T*[this->rows];
+    for (int i = 0; i < this->rows; ++i) {
+        this->board[i] = new T[this->columns];
+        for (int j = 0; j < this->columns; ++j) {
+            this->board[i][j] = ' ';
         }
-        delete[] board;
     }
-
-    bool update_board(int x, int y, char symbol) override {
-        if (x < 0 || x >= rows || y < 0 || y >= columns || board[x][y] != ' ')
-            return false;
-
-        board[x][y] = symbol;
-        n_moves++;
-        return true;
+}
+template <typename T>
+MisereTicTacToeBoard<T>::~MisereTicTacToeBoard() {
+    for (int i = 0; i < this->rows; ++i) {
+        delete[] this->board[i];
     }
-
-    void display_board() override {
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                cout << board[i][j];
-                if (j < columns - 1) cout << " ";
-            }
-            cout << endl;
-            if (i < rows - 1) {
-                cout << "-------------------" << endl;
-            }
-        }
-        cout << endl;
-    }
-
-    bool is_win() override {
-        // Check rows
-        for (int i = 0; i < rows; ++i) {
-            if (board[i][0] != ' ' &&
-                board[i][0] == board[i][1] &&
-                board[i][1] == board[i][2] &&
-                board[i][2] == board[i][3] &&
-                board[i][3] == board[i][4])
-                return true;
-        }
-
-        // Check columns
-        for (int j = 0; j < columns; ++j) {
-            if (board[0][j] != ' ' &&
-                board[0][j] == board[1][j] &&
-                board[1][j] == board[2][j] &&
-                board[2][j] == board[3][j] &&
-                board[3][j] == board[4][j])
-                return true;
-        }
-
-        // Check diagonals
-        if (board[0][0] != ' ' &&
-            board[0][0] == board[1][1] &&
-            board[1][1] == board[2][2] &&
-            board[2][2] == board[3][3] &&
-            board[3][3] == board[4][4])
-            return true;
-
-        if (board[0][4] != ' ' &&
-            board[0][4] == board[1][3] &&
-            board[1][3] == board[2][2] &&
-            board[2][2] == board[3][1] &&
-            board[3][1] == board[4][0])
-            return true;
-
+    delete[] this->board;
+}
+template <typename T>
+bool MisereTicTacToeBoard<T>::update_board(int x, int y, T symbol){
+    if (x < 0 || x >= this->rows|| y < 0 || y >=this-> columns || this->board[x][y] != ' ') {
         return false;
     }
 
-    bool is_draw() override {
-        return n_moves == rows * columns && !is_win();
-    }
+    this->board[x][y] = symbol;
+    this->n_moves++;
+    return true;
+}
 
-    bool game_is_over() override {
-        return is_win() || is_draw();
+template <typename T>
+void MisereTicTacToeBoard<T>::display_board() {
+    cout << "-------------" << endl;
+    for (int i = 0; i <  this->rows; i++) {
+        cout << "| ";
+        for (int j = 0; j <  this->columns; j++) {
+            cout <<  this->board[i][j] << " | ";
+        }
+        cout << endl << "-------------" << endl;
     }
-};*/
+}
+
+
+template <typename T>
+bool MisereTicTacToeBoard<T>::is_win() {
+
+    for (int i = 0; i < this->rows; i++) {
+        // Check rows
+        if (this->board[i][0] ==this-> board[i][1] && this->board[i][1] ==this-> board[i][2] && this->board[i][0] != ' ') {
+            return true;
+        }
+        // Check columns
+        if (this->board[0][i] ==this-> board[1][i] && this->board[1][i] == this->board[2][i] && this-> board[0][i] != ' ') {
+            return true;
+        }
+    }
+    // Check diagonals
+    if ((this->board[0][0] == this->board[1][1] && this->board[1][1] == this->board[2][2] && this->board[0][0] != ' ') ||
+        (this->board[0][2] == this->board[1][1] && this-> board[1][1] == this->board[2][0] && this->board[0][2] != ' ')) {
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool MisereTicTacToeBoard<T>::is_draw() {
+    return this->n_moves == this->rows * this->columns && !is_win();
+}
+
+template <typename T>
+bool MisereTicTacToeBoard<T>::game_is_over() {
+    return is_win() || is_draw();
+}
+template <typename T>
+class MisereTicTacToeHumanPlayer : public Player<char> {
+public:
+    MisereTicTacToeHumanPlayer (std::string n, T symbol);
+    void getmove(int &x, int &y);
+};
+
+template <typename T>
+MisereTicTacToeHumanPlayer <T>::MisereTicTacToeHumanPlayer (string n, T symbol) :Player<T>(n,symbol) {};
+
+template <typename T>
+void MisereTicTacToeHumanPlayer <T>::getmove(int &x, int &y) {
+    while (true) {
+        cout << this->name << ", enter your move (row and column, 0-2): "; // 3x3 board range
+        if (cin >> x >> y) {
+            if (x >= 0 && x < 3 && y >= 0 && y < 3) { // Validate for 3x3
+                return; // Valid move
+            } else {
+                cout << "Invalid move, try again." << endl;
+            }
+        } else {
+            cout << "Invalid input, try again." << endl;
+            cin >> x >> y;
+
+        }
+    }
+}
+
 template <typename T>
 class TicTacToeHumanPlayer : public Player<char> {
 public:
